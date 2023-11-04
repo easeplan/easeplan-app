6d24d262a31ac7edecd949c1a4713126da96a706
icode247 <ekekenta@tigrisdata.com>
2023-11-04 15:43:05 +0100

    few fixes

--- src/hooks/useFetchVendors.ts
+++ src/hooks/useFetchVendors.ts
@@ -1,5 +1,62 @@
 import { useState, useEffect } from 'react';
 
+type VendorData = {
+  _id: string;
+  userId: string;
+  publicId: string;
+  state: string;
+  city: string;
+  business: string;
+  identityVerify?: string;
+  introVideo?: string;
+  verified: boolean;
+  dob: string;
+  gender: string;
+  company: string;
+  samples: [
+    {
+      _id?: string;
+      image: string;
+      title: string;
+      description: string;
+    },
+  ];
+  budget: {
+    maximum: number;
+    minimum: number;
+  };
+  ratings: [];
+  rating: number;
+  selections: [
+    {
+      date: Date;
+      count: number;
+    },
+  ];
+  createdAt: Date;
+  events: Map<
+    string,
+    {
+      status: string;
+      bookedOn: string | null;
+      declined: boolean;
+      declinedReason?: string | null;
+      cancelReason?: string | null;
+      currentJob: string | null;
+    }
+  >;
+  balance: number;
+  role: string;
+  clients: string[];
+  responseTimes: number[];
+  currentlyHiredBy: string[];
+  currentlyRequestedBy: string[];
+  averageResponseTime: number;
+  availbalance: number;
+  approved: boolean;
+  registered: boolean;
+};
+
 export function useFetchVendors(
   page: unknown,
   search?: unknown,
@@ -8,7 +65,7 @@ export function useFetchVendors(
   budget?: any,
   service?: string,
 ) {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState<VendorData>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
@@ -18,7 +75,6 @@ export function useFetchVendors(
       `${process.env.NEXT_PUBLIC_API_URL}/profiles?page=${page}&state=${currentState}&city=${currentCity}&budget=${budget}&service=${service}&searchTerm=${search}`,
     )
       .then((response) => {
-        console.log(response);
         if (!response.ok) {
           throw new Error(`Network response was not ok`);
         }

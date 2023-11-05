5c4f214b5cfa54dd55ee8e4a5719304879f2b40f
icode247 <ekekenta@tigrisdata.com>
2023-11-05 22:41:09 +0100

    fix some error

--- src/pages/findvendors/index.tsx
+++ src/pages/findvendors/index.tsx
@@ -131,7 +131,7 @@ const VendorPage = ({ token }: Props) => {
 
   const { queryData } = useFetch(`/profiles/${userInfo}`, token);
   // const { isFetching, data: userData, error } = useGetCurrentUserQuery({ id });
-
+  const dataObj = data as any;
   return (
     <>
       <Head>
@@ -267,7 +267,7 @@ const VendorPage = ({ token }: Props) => {
               </Box>
             ) : (
               <>
-                {data?.length === 0 ? (
+                {dataObj?.data?.length === 0 ? (
                   <Box sx={{ textAlign: `center`, mt: 20, mb: 10 }}>
                     <HourglassEmptyIcon />
                     <Typography fontWeight={900} color="primary.main">
@@ -279,7 +279,7 @@ const VendorPage = ({ token }: Props) => {
                 )}
               </>
             )}
-            {data?.length === 0 ? null : (
+            {dataObj?.data?.length === 0 ? null : (
               <Box
                 sx={{
                   display: `flex`,
@@ -289,7 +289,7 @@ const VendorPage = ({ token }: Props) => {
                 }}
               >
                 <Pagination
-                  count={data?.totalPages}
+                  count={dataObj?.totalPages}
                   color="primary"
                   page={page}
                   onChange={handleChange}

63cd6fdac1d0d8d0ffa8df6dcbdb0b127a8e1391
Ekekenta  Odionyenfe <ekekentaodionyenfe@Ekekentas-MBP.lan>
2023-11-27 13:09:32 +0100

    few updates

--- src/components/vendors/SidenavDrawer.tsx
+++ src/components/vendors/SidenavDrawer.tsx
@@ -3,9 +3,6 @@ import Drawer from '@mui/material/Drawer';
 import { Avatar, Box, Button, IconButton, Tooltip } from '@mui/material';
 import NavItems from './NavItems';
 import CloseIcon from '@mui/icons-material/Close';
-import { RootState } from '@/store/store';
-import { useSelector } from 'react-redux';
-import { parseCookies } from '@/lib/parseCookies';
 import { useAuth } from '@/hooks/authContext';
 
 type Anchor = 'right';
